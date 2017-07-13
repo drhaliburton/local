a1290ce376d4bf1c2dbfa7910c1da66e4c0582cd
Holden Whitehead <holden.whitehead@yahoo.com>
2017-07-13 22:08:07 +0000

    Add empty routes to routes/itinerary. Comment-out skeleton code in app.js

--- routes/itinerary.js
+++ routes/itinerary.js
@@ -5,8 +5,25 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
-  router.get("/:id", (req, res) => {
-   
+  router.get("/:id/new", (req, res) => {
+   //New invenventory form
+   //Need user favorites
+  });
+
+  router.get("/:id/:date", (req, res) => {
+  //View specific Itinerary
+  //Link from Navbar?
+  });
+
+  router.get("/:id/:date/map", (req, res) => {
+  //View map containing all locations from an itinerary
+  //Give location cooridinates to Google maps api
+  });
+
+  router.post("/:id", (req, res) => {
+  // Save itinerary to DB
+  // Form will include favorite.card_id, itinerary-card.start_time, itinerary.date
+  // Delete each card used from favorite 
   });
 
   return router;
