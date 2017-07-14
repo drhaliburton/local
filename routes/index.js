cb6c57aed15a8a320bd7a491d53fa86c7d35d0ff
Holden Whitehead <holden.whitehead@yahoo.com>
2017-07-14 22:48:44 +0000

    Add test route for get /index and /itinerary with hard-coded sample data

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,47 @@
 
 const express = require('express');
 const router  = express.Router();
+const queries = require("../library/itinerary-queries.js")
+
+const testCards = {
+   cards: [{
+     title: "Food Place",
+     description: "Yum!",
+     location: "here",
+     type: "food",
+     duration: 120,
+     start_time: "11:00am"
+   }, {
+     title: "Hike Time",
+     description: "Wow!",
+     location: "There",
+     type: "outdoors",
+     duration: 180,
+     start_time: "1:00pm"
+   }, {
+     title: "Art Place",
+     description: "Cool!",
+     location: "Somewhere",
+     type:"sight",
+     duration: 60,
+     start_time: "4:00pm"
+   }]
+ };
+
 
 module.exports = (knex) => {
+  const {getIndex, getFiltered, makeFavorite} = queries(knex);
 
-  router.get("/:filter", (req, res) => {
-     // this route should be used to filter through the results
+  // Route will be "/:filter" once we implement geolocation
+  router.get("/", (req, res) => { 
+    // getIndex(location)
+    // .then(result => {
+    //   res.send(process(result))
+    // })
+    // .catch(err => {
+    //   res.status(400).send("oh god no");
+    // } 
+    res.render("index", testCards)
   });
 
   router.post("/favorites/:id", (req, res) => {
