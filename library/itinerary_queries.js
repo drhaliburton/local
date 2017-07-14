d92d4b48f6b4fafdb59a4462831ce04289d027ee
aishwaryaramachandran <aishwaryaramachandran1995@gmail.com>
2017-07-14 18:17:05 +0000

    Added preliminary knex queries for index and itinerary

--- library/itinerary_queries.js
+++ library/itinerary_queries.js
@@ -1,1 +1,19 @@
-// select itinerary with id x & date y
+
+module.exports = (knex) => {
+const obj = {};
+
+obj.getItinerary = function(user_id, date){
+  knex('card')
+  .where('user_id', user_id)
+  .and('date', date)
+
+}
+
+obj.getFavorite = function(user_id){
+  knex('favorite')
+  .where('user_id', user_id)
+}
+
+// add get map query after google api
+return obj;
+}
