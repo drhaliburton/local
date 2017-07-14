dd1342a0f25b26557c07f38075a4b3b48ebca288
Rebecca Haliburton <haliburtonre@gmail.com>
2017-07-14 21:10:59 +0000

    Added flickr API keys to .env

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,11 @@
 
 $(() => {
 
-
   $('.card-toggle').on('click', function() {
     $('.card-details').slideToggle();
     $('.fa-chevron-up').toggleClass('chev-rotate');
     $('.fa-chevron-up').toggleClass('chev-rotate-again');
     $('.card').toggleClass('expanded');
   })
+  
 });
