c1628fdc3a22e215d0881356119f6d61bffd7c54
Shauna Griffin <shaunahennessygriffin@gmail.com>
2017-07-14 18:48:00 +0000

    Added buttons for filters and toggle animation for showing and hiding filters

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,22 @@
 
 $(() => {
-
-
   $('.card-toggle').on('click', function() {
     $('.card-details').slideToggle();
   })
   $('.filter-toggle').on('click', function() {
-    $('.filter-details').slideToggle();
-  })
+    $('.filter-content').slideToggle(function() {
+      if($('.filter-content').is(':visible')){
+        $('.filter-toggle').on('click', function() {
+          $('.filter-brand').slideUp("slow", function(){
+          });
+        });
+      }
+    });
+  });
+  $('.filter-toggle').mouseover(function() {
+    if($('.filter-content').is(':hidden')){
+      $('.filter-brand').slideDown("medium", function() {
+      });
+    }
+  });
 });
