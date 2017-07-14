c14ca5d4fcb83f4894a0063932a076ec4aad8112
Shauna Griffin <shaunahennessygriffin@gmail.com>
2017-07-14 02:51:48 +0000

    Added content for filters and silde toggle function for filters

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,4 +5,7 @@ $(() => {
   $('.card-toggle').on('click', function() {
     $('.card-details').slideToggle();
   })
+  $('.filter-toggle').on('click', function() {
+    $('.filter-details').slideToggle();
+  })
 });
