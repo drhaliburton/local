cf3f60b405d06d960091e82ba2c426cd888b8379
Rebecca Haliburton <haliburtonre@gmail.com>
2017-07-14 18:40:55 +0000

    Completed detail toggle on index card

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,5 +4,8 @@ $(() => {
 
   $('.card-toggle').on('click', function() {
     $('.card-details').slideToggle();
+    $('.fa-chevron-up').toggleClass('chev-rotate');
+    $('.fa-chevron-up').toggleClass('chev-rotate-again');
+    $('.card').toggleClass('expanded');
   })
 });
