245dbbac4d6dde7a31f3defe9b256e2bc642ba8d
Rebecca Haliburton <haliburtonre@gmail.com>
2017-07-17 20:29:49 +0000

    Resolved merge conflicts

--- server.js
+++ server.js
@@ -1,6 +1,51 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+"use strict";
+
+require('dotenv').config();
+
+// const PORT        = process.env.PORT || 8080;
+const ENV         = process.env.ENV || "development";
+const express     = require("express");
+const bodyParser  = require("body-parser");
+const sass        = require("node-sass-middleware");
+const app         = express();
+
+
+const knexConfig  = require("./knexfile");
+const knex        = require("knex")(knexConfig[ENV]);
+const morgan      = require('morgan');
+const knexLogger  = require('knex-logger');
+const request     = require('request');
+
+
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config');
+
+// Seperated Routes for each Resource
+const itineraryRoutes = require("./routes/itinerary");
+const indexRoutes = require("./routes/index");
+
+// Load the logger first so all (static) HTTP requests are logged to STDOUT
+// 'dev' = Concise output colored by response status for development use.
+//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+app.use(morgan('dev'));
+
+// Log knex SQL queries to STDOUT as well
+app.use(knexLogger(knex));
+
+app.set("view engine", "jsx");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/styles", sass({
+  src: __dirname + "/styles",
+  dest: __dirname + "/public/styles",
+  debug: true,
+  outputStyle: 'expanded'
+}));
+app.use(express.static("public"));
+
+// Mount all resource routes
+app.use("/itinerary", itineraryRoutes(knex));
+app.use("/index",indexRoutes(knex));
 
 new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
@@ -8,10 +53,8 @@ new WebpackDevServer(webpack(config), {
       aggregateTimeout: 300,
       poll: 1000,
       ignored: /node_modules/
-    },
-    proxy: {'/index': 'http://localhost:3001'}
+    }
   })
-
   .listen(3000, '0.0.0.0', function (err, result) {
     if (err) {
       console.log(err);
