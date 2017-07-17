1e688557cb70eb52db086877a4e235363a034e28
Holden Whitehead <holden.whitehead@yahoo.com>
2017-07-17 18:24:25 +0000

    Add local_server as proxy -Moved routes and library to local_server -Copy .env to local_server -Add npm-debug.log to .gitignore -Create clean-up directory for unused files -Uninstalled unused dependancies -Removed conflicting foreign keys from seed files

---
+++ local_server/library/itinerary_queries.js
@@ -0,0 +1,22 @@
+
+module.exports = (knex) => {
+const obj = {};
+
+
+
+
+obj.getItinerary = function(user_id, date){
+  knex('card')
+  .where('user_id', user_id)
+  .and('date', date)
+
+}
+
+obj.getFavorite = function(user_id){
+  knex('favorite')
+  .where('user_id', user_id)
+}
+
+// add get map query after google api
+return obj;
+}
