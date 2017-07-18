7eb231f0323b22b2bdb36fa8b68ee07a1738dd28
Shauna Griffin <shaunahennessygriffin@gmail.com>
2017-07-18 23:04:53 +0000

    npm installed dependencies and added plugin to babel

--- src/index.jsx
+++ src/index.jsx
@@ -8,11 +8,8 @@ import HomepageIndex from './homepage/HomepageIndex.jsx';
 import ItineraryIndex from './itinerary/ItineraryIndex.jsx';
 import MapIndex from './map/MapIndex.jsx';
 import SignInIndex from './signin/SignInIndex.jsx';
-<<<<<<< HEAD
 import SortableComponent from './dnd/SortableComponent.jsx';
-=======
 // import Styles from '../public/styles/layout.css';
->>>>>>> 71a90256828909e07a6d378fa268d011e4d650e1
 
 
 
