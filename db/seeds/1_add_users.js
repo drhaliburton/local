12e3349fe6c19d77aec503ee2429e16cdeaefba7
Rebecca Haliburton <haliburtonre@gmail.com>
2017-07-18 01:34:53 +0000

    Updated migrations

--- db/seeds/1_add_users.js
+++ db/seeds/1_add_users.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD:db/seeds/users.js
-exports.seed = function(knex, Promise) {
-
-   
-Promise.all([
-        knex('users').insert({given_name: 'Aisha' , family_name: 'R'}),
-        knex('users').insert({given_name: 'Holden', family_name: 'W'}),
-        knex('users').insert({given_name: 'Rebecca', family_name: 'H'}),
-        knex('users').insert({given_name: 'Shauna', family_name: 'G'})
-      ])
-};
-=======
 exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
@@ -21,4 +9,3 @@ exports.seed = function(knex, Promise) {
       ])
     })
 };
->>>>>>> 234891b613aace3100e5a02dbf0576b64649acd5:db/seeds/1_add_users.js
