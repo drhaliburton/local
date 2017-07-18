aa0b9eeeb4aaf44349f50be63316544caab14c36
aishwaryaramachandran <aishwaryaramachandran1995@gmail.com>
2017-07-18 18:47:34 +0000

    Added login button for google auth

--- src/signin/SignInIndex.jsx
+++ src/signin/SignInIndex.jsx
@@ -1,19 +1,36 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import GoogleLogin from 'react-google-login';
+
+ // const successGoogle =
+ const failureGoogle = (response) => {
+  alert('Go home. Google doesnt like you')
+ }
+
+ const responseGoogle = (response) => {
+  document.getElementById('googleButton')
+  console.log(response);
+  fetch('/auth', {
+    method: 'POST',
+    body: JSON.stringify({
+      tokenID: response.tokenId
+    })
+
+  })
+}
+
 
 class SignIn extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cards: []
-    }
-  }
+
+
 
   render() {
-      console.log("Rendering <App />");
     return (
-      <div>    
-        <h1>Sign In</h1>
-      </div>
+        <GoogleLogin
+        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={failureGoogle}/>
     );
   }
 }
