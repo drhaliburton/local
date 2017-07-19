964e0ea9457abc6fd39c60616209ab04afcedb30
Rebecca Haliburton <haliburtonre@gmail.com>
2017-07-19 19:46:36 +0000

    Fixing merge issues

--- server.js
+++ server.js
@@ -7,13 +7,11 @@ const ENV         = process.env.ENV || "development";
 const express     = require("express");
 const bodyParser  = require("body-parser");
 const sass        = require("node-sass-middleware");
-const app         = express();
 
 const knexConfig  = require("./knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
-const bodyParser = require("body-parser");
 const PORT = 3000;
 // Seperated Routes for each Resource
 const itineraryRoutes = require("./routes/itinerary");
