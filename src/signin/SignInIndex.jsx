b9f288a4f923591a5303b8677ac732d3a1ca1547
aishwaryaramachandran <aishwaryaramachandran1995@gmail.com>
2017-07-19 00:03:09 +0000

    Added sign in feature

--- src/signin/SignInIndex.jsx
+++ src/signin/SignInIndex.jsx
@@ -1,36 +1,35 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
 import GoogleLogin from 'react-google-login';
-
- // const successGoogle =
- const failureGoogle = (response) => {
-  alert('Go home. Google doesnt like you')
- }
-
- const responseGoogle = (response) => {
-  document.getElementById('googleButton')
-  console.log(response);
-  fetch('/auth', {
-    method: 'POST',
-    body: JSON.stringify({
-      tokenID: response.tokenId
-    })
-
-  })
-}
-
 
 class SignIn extends Component {
 
+  responseGoogle (response) {
+    document.getElementById('googleButton')
+    console.log(JSON.stringify(response.tokenId))
+    fetch('/auth', {
+      method: 'POST',
+      headers: JSON.stringify({
+        Authorization: response.tokenId
+      })
 
+    })
+  }
+
+  failureGoogle (response){
+    alert('Go home. Google doesnt like you')
+  }
+
+  componentDidMount() {
+
+  }
 
   render() {
     return (
         <GoogleLogin
         clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
         buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={failureGoogle}/>
+        onSuccess={this.responseGoogle}
+        onFailure={this.failureGoogle}/>
     );
   }
 }
