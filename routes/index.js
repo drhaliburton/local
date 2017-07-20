769f214867e060a48860eca87f8ce1bef3808cf9
Holden Whitehead <holden.whitehead@yahoo.com>
2017-07-20 19:18:42 +0000

    Clean up code

--- routes/index.js
+++ routes/index.js
@@ -12,23 +12,8 @@ module.exports = (knex) => {
     allCards
   } = queries(knex);
 
-  // Route will be "/:filter" once we implement geolocation
   router.get("/", (req, res) => {
-    //The following response will be used once geolocation has been implemented
 
-    // const lat1 = req.body.results[0].geometry.viewport.northeast.lat;
-    // const lng1 =req.body.results[0].geometry.viewport.northeast.lng;
-    // const lat2 =req.body.results[0].geometry.viewport.southwest.lat;
-    // const lng2 =req.body.results[0].geometry.viewport.southwest.lng;
-    // getFiltered(lat1, lng1, lat2, lng2)
-    // .then(data => {
-    //   res.render("index", {cards: data})
-    // })
-    // .catch(err => {
-    //   res.status(400).send("ERROR");
-    // })
-
-    //This is a temporary response, for testing purposes
     allCards()
       .then(data => {
         let cards = data.map((card) => {
