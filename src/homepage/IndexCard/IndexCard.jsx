de20bdee4a5133a6609a0665518d786ffbc13885
Shauna Griffin <shaunahennessygriffin@gmail.com>
2017-07-20 02:42:53 +0000

    Fixed error with double naming of function and component

--- src/homepage/IndexCard/IndexCard.jsx
+++ src/homepage/IndexCard/IndexCard.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import ContentContainer from "./ContentContainer.jsx";
+import NewImage from "./NewImage.jsx";
 //Contains
 class IndexCard extends Component {
   render() {
@@ -8,7 +9,7 @@ class IndexCard extends Component {
       <div className="columns is-multiline cards">
         {cardsArray.map((card, index) => {
           return <div key={index} className="column is-one-quarter card">
-              <Image key={`${card.id}_img`} image={card.image}/>
+              <NewImage key={`${card.id}_img`} image={card.image}/>
               <ContentContainer key={card.id} cardID={card.id} cardContent={card} favorite={this.props.favorite} />
             </div>;
           })
