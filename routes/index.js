574ab4a89907a0f0354a387f373a290653e8c352
aishwaryaramachandran <aishwaryaramachandran1995@gmail.com>
2017-07-22 19:42:27 +0000

    changes

--- routes/index.js
+++ routes/index.js
@@ -21,19 +21,24 @@ function createFlickrUrl(photoArray) {
 module.exports = (knex) => {
   const {
     getFiltered,
+    makeFavorite,
     allCards,
-    addFavorite
+    postUpvote,
+    postDownvote,
+    getVotes
   } = queries(knex);
 
   const {
     postPhotos,
-    postCard,
-    findPlacePhotos
+    postCard
   } = cardQueries(knex);
 
-
+  // Route will be "/:filter" once we implement geolocation
   router.get("/", (req, res) => {
+    //The following response will be used once geolocation has been implemented
 
+
+    //This is a temporary response, for testing purposes
     allCards()
       .then(data => {
         let cards = data.map((card) => {
@@ -46,7 +51,7 @@ module.exports = (knex) => {
             category: card.category_name,
             user: card.given_name,
             photos: card.photo_url,
-            ratings: card.total_rating
+            ratings: card.rating
           }
         });
         res.json(cards)
@@ -79,6 +84,7 @@ module.exports = (knex) => {
       //the whole response has been recieved, so we just print it out here
       response.on('end', function () {
         const result = JSON.parse(str).results[0];
+        console.log(result);
 
         const lat1 = result.geometry.viewport.northeast.lat;
         const lng1 = result.geometry.viewport.northeast.lng;
@@ -95,7 +101,7 @@ module.exports = (knex) => {
                 duration: card.duration,
                 category: card.name,
                 user: card.given_name,
-                photos: card.photo_url,
+                photos: card.url,
                 ratings: card.rating
               }
             })
@@ -110,72 +116,25 @@ module.exports = (knex) => {
     https.request(options, callback).end();
   })
 
+
   router.post("/", (req, res) => {
 
-    const newCard = {
-      title: req.body.title,
-      description: req.body.description,
-      duration: req.body.duration,
-      category: req.body.category,
-      user_id: req.session.userId
-    }
-    const geoKey = process.env.GEO_API_KEY
-    const request = encodeURIComponent(req.body.location)
-    const options = {
-      host: 'maps.googleapis.com',
-      path: `/maps/api/geocode/json?address=${request}&key=${geoKey}`
-    };
+    // postCard(req.body);
+    res.status(200).send("Okay");
+    // postPhotos(images);
 
-    const callback = function (response) {
-      let str = '';
-
-      //another chunk of data has been recieved, so append it to `str`
-      response.on('data', function (chunk) {
-        str += chunk;
-      });
-
-      //the whole response has been recieved, so we just print it out here
-      response.on('end', function () {
-        const result = JSON.parse(str).results[0];
-        newCard.location = `(${result.geometry.location.lat}, ${result.geometry.location.lng})`
-        const photosArray = findPlacePhotos(result);
-
-      postCard(newCard)
-        .then(([cardID]) => {
-          return photosArray
-          .then((images) => {
-            postPhotos(images, cardID);
-          })
-          .then(() => {
-            res.json({
-              status: 'ok'
-            });
-          })
-        })
-        .catch(err => {
-          res.status(400).send("ERROR");
-        })
-      })
-    }
-    https.request(options, callback).end();
   });
 
+
+
+
   router.post("/favorite", (req, res) => {
-    const userId = req.session.userId;
-    const cardId = req.body.id;
-    addFavorite(cardId, userId)
-      .then(() => {
-        res.json({
-          status: 'ok'
-        })
-      })
-      .catch(err => {
-        res.status(400).send("ERROR");
-      });
-  })
+    console.log(req.body.id)
+    res.json({
+      status: 'ok'
+    });
+  });
 
 
   return router;
 }
-
-
