fc49853f14b505519e35e9da6f3260ca62d5bdda
Shauna Griffin <shaunahennessygriffin@gmail.com>
2017-07-22 22:20:07 +0000

    Added styling to button

--- src/signin/SignInIndex.jsx
+++ src/signin/SignInIndex.jsx
@@ -50,6 +50,7 @@ class SignInIndex extends Component {
           <GoogleLogin
             clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
             buttonText="Login"
+            className='button'
             scope={'https://www.googleapis.com/auth/calendar'}
             onSuccess={this.responseGoogle.bind(this)}
             onFailure={this.failureGoogle.bind(this)} />
