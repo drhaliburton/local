3ef5b769bf9580c7cfb3625e328bf2e6437a5f2a
Rebecca Haliburton <haliburtonre@gmail.com>
2017-07-22 20:01:12 -0700

    Nuka carousel working

--- src/itinerary/FavSlider.jsx
+++ src/itinerary/FavSlider.jsx
@@ -1,55 +1,63 @@
 import React, {Component} from 'react';
-import Slider from 'react-slick';
-import FavCard from "./FavCard/FavCard.jsx";
+import FavContentContainer from "./FavCard/FavContentContainer.jsx";
 
-var FavSlider = React.createClass({
-  render: function() {
-  	var settings = {
-      dots: true,
-      arrows: true,
-      infinite: true,
-      speed: 500,
-      initialSlide: 8
-    }
+
+
+var Carousel = require('nuka-carousel');
+
+const FavSlider = React.createClass({
+  mixins: [Carousel.ControllerMixin],
+  render() {
+    let cardsArray = this.props.cards;
     return (
-    	<div className='cards'>
-      	<Slider {...settings}>
-          <div></div>
-            <FavCard cards={this.props.cards} />
-        </Slider>
-      </div>
-    );
+      <Carousel slidesToShow={3} slidesToScroll={2} cellSpacing={20} dragging={true} easing="easeInOut" slideWidth="200px" width="90%">
+         {
+          cardsArray.map((card, index) => {
+            return <div key={index} className="card">
+              <FavContentContainer key={card.id} cardContent={card}/>
+            </div>;
+          })
+        }
+      </Carousel>
+    )
   }
 });
 
-// class FavSlider extends React.Component {
+module.exports = FavSlider;
 
-//   componentDidMount() {
 
-//   }
 
-//   render() {
-//     const settings = {
+
+
+
+// import React, {Component} from 'react';
+// import Slider from 'react-slick';
+// import FavCard from "./FavCard/FavCard.jsx";
+
+// var FavSlider = React.createClass({
+//   render: function() {
+//   	var settings = {
 //       dots: true,
 //       arrows: true,
 //       infinite: true,
+//       centerPadding: true,
 //       speed: 500,
-//       accessibility: true,
-//       slidesToShow: 5,
+//       initialSlide: 1,
 //       slidesToScroll: 1
-//     };
+//     }
+
 
 //     return (
-//       <div className="cards">
-//         <button onClick={this.slickNext()}></button>
-//         <Slider {...settings}>
-//           <div></div>
+//     	<div className='cards'>
+//       	<Slider {...settings}>
+//             <div></div>
+//             <FavCard cards={this.props.cards} />
 
 //         </Slider>
 //       </div>
 //     );
 //   }
-// }
+// });
 
-export default FavSlider;
+// export default FavSlider;
 
