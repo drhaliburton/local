f54ba3464f94ed9aee45eb04827b6c426c5a7c9d
Rebecca Haliburton <haliburtonre@gmail.com>
2017-07-24 13:32:42 -0700

    Fixed index page star

--- src/homepage/IndexCard/Star.jsx
+++ src/homepage/IndexCard/Star.jsx
@@ -1,8 +1,23 @@
 import React, {Component} from 'react';
 
 class Star extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        isFavorited: false
+      }
+    }
+
+  toggleFavorite(event){
+    this.setState({
+      isFavorited: !this.state.isFavorited
+    });
+  }
+
 
   render() {
+
+    const favToggle = this.state.isFavorited ? 'fa-star is-full' : 'fa-star-o is-open';
     return (
       <span className="icon is-pulled-right" aria-hidden="true" onClick={this.toggleFavorite.bind(this)}><i className={`fa ${favToggle}`} onClick={(event) => {
                   this.props.favorite({id: this.props.cardID})}}></i></span>
