65d4a116947fea4856cf03932e8bb19bbcf89bbd
Rebecca Haliburton <haliburtonre@gmail.com>
2017-07-24 16:00:41 -0700

    Fixed navbar collapse styling

--- src/nav/Navbar.jsx
+++ src/nav/Navbar.jsx
@@ -22,8 +22,6 @@ class Navbar extends Component {
         <div className="navbar-item">
           <a href="/"><img src="http://imgur.com/cyM42ng.png" alt="local - travel like you live there" width="75" height="28"></img></a>
         </div>
-
-        <div className="navbar-end"></div>
         <div className="navbar-brand">
           {currentRoute.includes('map') ?
             <div className="navbar-item">
@@ -73,6 +71,7 @@ class Navbar extends Component {
             </div>
           }
         </div>
+      <div className="navbar-end"></div>
       </nav>
     );
   }
